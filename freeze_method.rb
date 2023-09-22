514ea0f23edc9fe23a45fbc5efef2b252280a5fe
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-22 10:00:11 -0400

    Add files via upload

---
+++ freeze_method.rb
@@ -0,0 +1,9 @@
+#if we freeze an object, we make it immutable 
+#when we freeze an object we're saying it cant change 
+
+name = "Naima".freeze 
+hobbies = ["Coding", "Sushi"].freeze 
+
+name << "winning"
+
+#can't modify array 
