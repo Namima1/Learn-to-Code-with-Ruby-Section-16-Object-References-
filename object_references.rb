94bc17e6d04e02448d665276b363d7834208e75d
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-24 15:55:36 -0400

    Add files via upload

---
+++ object_references.rb
@@ -0,0 +1,12 @@
+a = [1, 2, 3]
+b = a 
+
+#this is giving a new name to the same array  
+ p a 
+ p b 
+
+ p a.object_id 
+ p b.object_id
+ p [1, 2, 3].object_id
+ p [1, 2, 3].object_id
+ #each time ruby is creating a new array 
